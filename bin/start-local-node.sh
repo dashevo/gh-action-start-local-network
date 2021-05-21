c345608ff1e1f3a8eeba072b6702e22e73e926c9
strophy <badfunkstripe@gmail.com>
2021-05-21 12:05:42 +0200

    chore(ci): pass debug output var to dashmate

--- bin/start-local-node.sh
+++ bin/start-local-node.sh
@@ -96,13 +96,20 @@ dashmate update
 echo "Setting up a local network"
 
 NODE_COUNT=3
+DEBUG_OUTPUT=true
+DASHMATE_VERSION=$(jq -r '.version' $GITHUB_WORKSPACE/package.json)
 
 dashmate config:set --config=local environment development
 dashmate config:set --config=local platform.drive.abci.log.stdout.level trace
 
-dashmate setup local --verbose --node-count="$NODE_COUNT" | tee setup.log
-
+if [[ $DASHMATE_VERSION =~ ^0\.20* ]]; then
+  dashmate setup local --verbose --node-count="$NODE_COUNT" --debug-output="$DEBUG_OUTPUT" | tee setup.log
+else
+  dashmate setup local --verbose --node-count="$NODE_COUNT" | tee setup.log
+fi
 #  Save npm cache
+
+mn config --config=local_1
 
 if [ -n "$drive_branch" ]
 then
